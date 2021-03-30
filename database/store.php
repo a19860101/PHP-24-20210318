cd6f7f7e1b7aa01f0319a0d721137c4af45f16ab
Zac <a19860101@gmail.com>
2021-03-30 20:52:56 +0800

    20210330

--- database/store.php
+++ database/store.php
@@ -2,17 +2,23 @@
 
     // print_r($_POST);
 
-    echo $_POST["name"];
-    echo "<br>";
-    echo $_POST["phone"];
-    echo "<br>";
-    echo $_POST["mail"];
-    echo "<br>";
-    echo $_POST["gender"];
-    echo "<br>";
-    echo $_POST["edu"];
-    echo "<br>";
-    print_r($_POST["skill"]);
-    echo "<br>";
-    echo $_POST["content"];
-    echo "<br>";
+    // echo $_POST["name"];
+    // echo "<br>";
+    // echo $_POST["phone"];
+    // echo "<br>";
+    // echo $_POST["mail"];
+    // echo "<br>";
+    // echo $_POST["gender"];
+    // echo "<br>";
+    // echo $_POST["edu"];
+    // echo "<br>";
+    // print_r($_POST["skill"]);
+    // echo "<br>";
+    // echo $_POST["content"];
+    // echo "<br>";
+
+    extract($_POST);
+    echo implode(",",$skill);
+
+    // extract($_GET);
+    // echo implode(",",$skill);
