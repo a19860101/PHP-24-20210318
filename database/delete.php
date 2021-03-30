23ed07e0d784e9067c29df93cb6328697f362cc2
Zac <a19860101@gmail.com>
2021-03-30 21:49:17 +0800

    20210330

--- database/delete.php
+++ database/delete.php
@@ -1,2 +1,9 @@
 <?php
-    echo $_POST["id"];
+    require_once("conn.php");
+    extract($_POST);
+
+    $sql = "DELETE FROM students WHERE id = $id";
+    mysqli_query($conn,$sql);
+
+    echo "資料已刪除";
+    header("refresh:2;url=index.php");
