ee25ab54cf6320f07efb09e6638b36f86083d363
Zac <a19860101@gmail.com>
2021-04-06 20:37:32 +0800

    20210406

--- pdo/store.php
+++ pdo/store.php
@@ -1,35 +1,32 @@
 <?php
-    require_once("conn.php");
-    extract($_POST);
-
-    print_r($_POST);
-
-    if(!isset($_POST["skill"])){
-        $skill = '';
-        // echo "請勾選專長";
-        // header("refresh:1;url=create.php");
-        // return;
-    }else{
-        $skill = implode(",",$skill);
-    }
-
-    if(!isset($_POST["gender"])){
-        $gender = '';
-    }
-
-
-    $sql = "INSERT INTO students(name,phone,mail,gender,edu,skill,content,created_at)
-            VALUES(?,?,?,?,?,?,?,NOW())";
+    try {
+        require_once("pdo.php");
+        extract($_POST);
+        
+        if(!isset($_POST["skill"])){
+            $skill = '';
+            // echo "請勾選專長";
+            // header("refresh:1;url=create.php");
+            // return;
+        }else{
+            $skill = implode(",",$skill);
+        }
+        
+        if(!isset($_POST["gender"])){
+            $gender = '';
+        }
+        
+        $sql = "INSERT INTO students(name,phone,mail,gender,edu,skill,content,created_at)
+            VALUES(?,?,?,?,?,?,?,?)";
     
-    $stmt = $conn->prepare($sql);
-    
-    $stmt->bind_param("sssssss",$name,$phone,$mail,$gender,$edu,$skill,$content);
+        $stmt = $pdo->prepare($sql);
+        
+        $now = date("Y-m-d H:i:s");
 
-    /*
-        s: string
-        i: integer
-    */
-    $stmt->execute();
-
-    echo "資料已新增";
-    header("refresh:2;url=index.php");
+        $stmt->execute([$name,$phone,$mail,$gender,$edu,$skill,$content,$now]);
+        
+        echo "資料已新增";
+        header("refresh:2;url=index.php");
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
