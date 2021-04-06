739eb3cc5ebee8cb71ed24cdf903d4818045f22c
Zac <a19860101@gmail.com>
2021-04-06 22:01:57 +0800

    20210406

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -6,5 +6,8 @@
     print_r($_FILES["pic"]["name"]);
 
     extract($_FILES["pic"]);
-    echo "<br>";
-    echo $type;
+    // echo "<br>";
+    // echo $type;
+    if($error == 0){
+        move_uploaded_file($tmp_name,"images/".$name);
+    }
