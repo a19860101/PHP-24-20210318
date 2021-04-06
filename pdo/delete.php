ae53533ad1ff8d95b8a5a209f26ee4f73350ecae
Zac <a19860101@gmail.com>
2021-04-06 21:37:37 +0800

    20210406

--- pdo/delete.php
+++ pdo/delete.php
@@ -4,9 +4,15 @@
         require_once("pdo.php");
         extract($_POST);
 
-        $sql = "DELETE FROM students WHERE id = ?";
+        // $sql = "DELETE FROM students WHERE id = ?";
+        // $stmt = $pdo->prepare($sql);
+        // $stmt->execute([$id]);
+        
+        $sql = "DELETE FROM students WHERE id = :id";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([$id]);
+        $stmt->bindParam(":id",$id);
+        $stmt->execute();
+
 
         echo "資料已刪除";
         header("refresh:1;url=index.php");
