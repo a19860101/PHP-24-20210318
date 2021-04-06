37bf3f15f7d3cd8a55512a98f3dc2d1a5fa473c1
Zac <a19860101@gmail.com>
2021-04-06 21:26:38 +0800

    20210406

--- pdo/store.php
+++ pdo/store.php
@@ -16,14 +16,25 @@
             $gender = '';
         }
         
-        $sql = "INSERT INTO students(name,phone,mail,gender,edu,skill,content,created_at)
-            VALUES(?,?,?,?,?,?,?,?)";
-    
-        $stmt = $pdo->prepare($sql);
-        
+        // $sql = "INSERT INTO students(name,phone,mail,gender,edu,skill,content,created_at)
+        //     VALUES(?,?,?,?,?,?,?,?)";
+        // $stmt = $pdo->prepare($sql);
+        // $stmt->execute([$name,$phone,$mail,$gender,$edu,$skill,$content,$now]);
 
-        $stmt->execute([$name,$phone,$mail,$gender,$edu,$skill,$content,$now]);
-        
+        $sql = "INSERT INTO students(name,phone,mail,gender,edu,skill,content,created_at)
+                VALUES(:name,:phone,:mail,:gender,:edu,:skill,:content,:created_at)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(":name",$name);
+        $stmt->bindParam(":phone",$phone);
+        $stmt->bindParam(":mail",$mail);
+        $stmt->bindParam(":gender",$gender);
+        $stmt->bindParam(":edu",$edu);
+        $stmt->bindParam(":skill",$skill);
+        $stmt->bindParam(":content",$content);
+        $stmt->bindParam(":created_at",$now);
+        $stmt->execute();
+
+
         echo "資料已新增";
         header("refresh:2;url=index.php");
     }catch(PDOException $e){
