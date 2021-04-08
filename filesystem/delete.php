b3f5286a3abdc1c451f3b79ff1733173f572d90a
Zac <a19860101@gmail.com>
2021-04-08 21:45:36 +0800

    20210408

--- filesystem/delete.php
+++ filesystem/delete.php
@@ -1,7 +1,17 @@
 <?php
+    require_once("pdo.php");
     extract($_POST);
     echo $img;
+    
+    // 只有資料夾，沒有經過資料庫
+    // unlink($img);
 
-    unlink($img);
+    // 資料庫
+    unlink("images/".$img);
+
+    $sql = "DELETE FROM imgs WHERE id = ?";
+    $stmt=$pdo->prepare($sql);
+    $stmt->execute([$id]);
+
 
     header("location:list.php");
