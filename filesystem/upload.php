c16011f6a97979f4359e08b89456d8c0e9cbc2b5
Zac <a19860101@gmail.com>
2021-04-08 19:28:28 +0800

    20210408

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -1,13 +1,42 @@
 <?php
-    print_r($_FILES);
-    echo "<br>";
-    print_r($_FILES["pic"]);
-    echo "<br>";
-    print_r($_FILES["pic"]["name"]);
+    // print_r($_FILES);
+    // echo "<br>";
+    // print_r($_FILES["pic"]);
+    // echo "<br>";
+    // print_r($_FILES["pic"]["name"]);
 
     extract($_FILES["pic"]);
+
+    $ext = strtolower(pathinfo($name,PATHINFO_EXTENSION));
+    // $ext = pathinfo($name,PATHINFO_EXTENSION);
+    $filename = pathinfo($name,PATHINFO_FILENAME);
+    $basename = pathinfo($name,PATHINFO_BASENAME);
+    $dirname = pathinfo($name,PATHINFO_DIRNAME);
+
+    // echo $ext;
+    // echo "<br>";
+    // echo $filename;
+    // echo "<br>";
+    // echo $basename;
+    // echo "<br>";
+    // echo $dirname;
+    // echo "<br>";
+
+    if($ext != "jpg" && $ext != "jpeg" && $ext != "png" && $ext != "gif" ){
+        echo "檔案類型錯誤。請上船正確的圖片檔案";
+    }
+
     // echo "<br>";
     // echo $type;
-    if($error == 0){
-        move_uploaded_file($tmp_name,"images/".$name);
-    }
+    // if($error == 0){
+        // if(move_uploaded_file($tmp_name,"images/".$name)){
+            // echo "上傳成功";
+            // header("Refresh:1;url=index.php");
+        // }
+    // }
+    /*
+        只有圖片可以上傳
+        檔名要亂數
+        如果資料夾不存在
+    
+    */
