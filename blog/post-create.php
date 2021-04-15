3a79ff55bb84c500c12bebaa945d6c596420b8bc
Zac <a19860101@gmail.com>
2021-04-15 19:22:03 +0800

    20210415

--- blog/post-create.php
+++ blog/post-create.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
-<body>
-    <?php include("template/nav.php"); ?>
 
-</body>
-</html>
+<?php include("template/header.php"); ?>
+<?php include("template/nav.php"); ?>
+<?php include("template/footer.php"); ?>
