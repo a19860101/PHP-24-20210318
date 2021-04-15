45062c17cf7b9a90155ab5673dc897abf9819730
Zac <a19860101@gmail.com>
2021-04-15 19:12:21 +0800

    20210415

---
+++ blog/pdo.php
@@ -0,0 +1,18 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "admin";
+    $db_pw = "admin";
+    $db_name = "php-24-20210318";
+    $db_charset = "utf8";
+
+    date_default_timezone_set("Asia/Taipei");
+    $now = date("Y-m-d H:i:s");
+
+
+    try {
+        $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";        
+        $pdo = new PDO($dsn,$db_user,$db_pw);
+        
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
---
+++ blog/template/nav.php
@@ -0,0 +1,13 @@
+<nav>
+    <div class="nav-logo">
+        <a href="index.php">APPLE BLOG</a>
+    </div>
+    <div class="nav-left">
+        <a href="post-create.php">新增文章</a>
+    </div>
+    <div class="nev-right">
+        <a href="#">登入</a>
+        <a href="#">註冊會員</a>
+        <a href="#">登出</a>
+    </div>
+</nav>
