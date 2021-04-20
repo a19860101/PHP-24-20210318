2d320b21157fccfce48a100c619ae956656726b6
Zac <a19860101@gmail.com>
2021-04-20 19:38:37 +0800

    20210420

--- blog/index.php
+++ blog/index.php
@@ -15,13 +15,14 @@
         <?php foreach($posts as $post){ ?>
         <div class="col-10 border border-dark mb-3 p-5 rounded">
             <h2><?php echo $post["title"];?></h2>
-            <div class="content">
+            <div class="content my-3">
                 <?php 
                     $content = strip_tags($post["content"]);
                     echo mb_substr($content,0,150);
                 ?>...
                 <br>
-                <a href="post-show.php?id=<?php echo $post["id"];?>" class="btn btn-primary">繼續閱讀</a>
+                <br>
+                <a href="post-show.php?id=<?php echo $post["id"];?>" class="btn btn-primary ">繼續閱讀</a>
             </div>
             <div>
                 最後更新時間: <?php echo $post["updated_at"];?>
--- blog/template/nav.php
+++ blog/template/nav.php
@@ -6,7 +6,7 @@
         <a href="post-create.php">新增文章</a>
     </div>
     <div class="nev-right">
-        <a href="#">登入</a>
+        <a href="login.php">登入</a>
         <a href="#">註冊會員</a>
         <a href="#">登出</a>
     </div>
