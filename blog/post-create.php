e543f4e4f3a776ac0e6a3b3734c1bd0cd093b4af
Zac <a19860101@gmail.com>
2021-04-29 20:03:49 +0800

    20210429

--- blog/post-create.php
+++ blog/post-create.php
@@ -102,10 +102,30 @@
         menubar: false,
         plugins: 'image code link',
         toolbar: 'undo redo | image code link| styleselect | bold italic forecolor underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent',
-        images_upload_url: '',
+        images_upload_url: 'postAcceptor.php',
         automatic_uploads: true,
         image_title: true,
-        file_picker_types: 'image'
+        file_picker_types: 'image',
+        file_picker_callback: function (cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+        input.onchange = function () {
+        var file = this.files[0];
+        var reader = new FileReader();
+        reader.onload = function () {
+            var id = 'blobid' + (new Date()).getTime();
+            var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+            var base64 = reader.result.split(',')[1];
+            var blobInfo = blobCache.create(id, file, base64);
+            blobCache.add(blobInfo);
+            cb(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+        };
+
+        input.click();
+    },
 
     });
     $(function(){
